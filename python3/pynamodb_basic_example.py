32ffb282b14a4e6518d853feeadb6059de926e3c
kmilo.denis <kmilo.denis.glez@gmail.com>
2023-07-03 08:04:16 -0500

    update

--- python3/pynamodb_basic_example.py
+++ python3/pynamodb_basic_example.py
@@ -16,7 +16,6 @@ from pynamodb.models import Model
 from pynamodb.attributes import UnicodeAttribute, UTCDateTimeAttribute, DynamicMapAttribute, BooleanAttribute, \
     NumberAttribute
 
-
 aws_region = os.getenv('AWS_REGION_SERVER') or 'us-east-1'
 db_settings_table_name = os.getenv('DB_TABLE_SETTINGS') or 'settings_table'
 
@@ -60,11 +59,69 @@ class Settings(Model):
     data = SettingData()
 
 
+# get the first item in settings table
+def get_first_item():
+    results = Settings.scan(limit=1)
+    try:
+        item = next(results)
+    except ScanError as ex:
+        raise ex
+
+    return item
+
+
+# Scan filters example
+def iterate_all_item():
+    # Using only 5 RCU per second
+    results = Settings.scan(rate_limit=5)
+    while True:
+        try:
+            item = next(results)
+        except StopIteration:
+            break
+        except ScanError as ex:
+            raise ex
+        else:
+            # handle item
+            print(item)
+
+
+def update_using_params_example(model_instance: Model):
+    # create a data object for -- update -- item
+    data_setting_object = SettingData(
+        debug_mode_enabled=False,
+        notification_enabled=False
+    )
+
+    # -- update -- setting item [data.debug_mode_enabled, data.notification_enabled and updated_at]
+    model_instance.update(
+        actions=[
+            Settings.updated_at.set(datetime.utcnow()),
+            Settings.data.set(data_setting_object)
+        ]
+    )
+
+
+def update_using_dict_example(model_instance: Model):
+    data_setting_dict = {
+        'debug_mode_enabled': True,
+        'notification_enabled': False
+    }
+    settings_data_object = SettingData(**data_setting_dict)
+
+    model_instance.update(
+        actions=[
+            Settings.updated_at.set(datetime.utcnow()),
+            Settings.data.set(settings_data_object)
+        ]
+    )
+
+
 if __name__ == '__main__':
     # create data object for -- insert -- item
     # omit the 'debug_mode_enabled' attribute because its default value is false
     data_setting1 = SettingData(
-        notification_enabled=10
+        notification_enabled=True
     )
 
     # generate slug partition-key attribute using uuid4
@@ -80,19 +137,13 @@ if __name__ == '__main__':
     print("Show setting object: ", settings1)
     print("Show setting object like a JSON-serializable: ", settings1.to_simple_dict())
 
-    # create a data object for -- update -- item
-    data_setting_update = SettingData(
-        debug_mode_enabled=True,
-        notification_enabled=23
-    )
+    update_using_dict_example(settings1)
 
-    # -- update -- setting item [data.debug_mode_enabled, data.notification_enabled and updated_at]
-    settings1.update(
-        actions=[
-            Settings.updated_at.set(datetime.utcnow()),
-            Settings.data.set(data_setting_update)
-        ]
-    )
+    print("Show setting object: ", settings1)
+
+    update_using_params_example(settings1)
+
+    print("Show setting object: ", settings1)
 
     # -- delete -- item
     # settings1.delete()
